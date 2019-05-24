1bf45705c5f762b21df7ed95fa65fefdca298a11
Martin Pritchard <martin@martinpritchard.com>
2019-05-24 22:19:18 +0100

    Allocate memory & registers
    - Allocate space to represent the CHIP-8 system
    - Add outline of what we're aiming for

--- chip8.c
+++ chip8.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
 
+// TODO:
+// 1. Startup
+// 2. Initialise CPU
+// 3. Load ROM
+// 4. Start loop
+// 5. Emulate CPU cycle
+// 6. Update screen if draw flag set
+// 7. Read keypad
+// 8. Goto step 5
+
 int main()
 {
-   printf( "Hello World" );
-   return 0;
+    unsigned char memory[ 4096 ]; // 4K 8-bit memory
+    unsigned char V[ 16 ];        // 16 8-bit registers (V0-VF)
+    unsigned short I;             // 16-bit Index register
+    unsigned short pc;            // Program counter: 0x000 - 0xFFF
+    unsigned short opcode;
+
+    unsigned char gfx[ 64 * 32 ]; // 64x32 pixel display
+
+    unsigned char delay_timer;
+    unsigned char sound_timer;
+
+    unsigned short stack[ 16 ];
+    unsigned short stack_ptr;
+
+    unsigned char keypad[ 16 ];
 }
